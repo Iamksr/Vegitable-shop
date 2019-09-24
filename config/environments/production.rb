de6bac0a885ae1cc475eff4cea234ff8c49d571d
Kapilgoyal6 <kapilgoyal6@gmail.com>
2019-09-24 18:03:42 +0530

    issue fixed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+    config.action_mailer.default_url_options = { host: 'https://aidencommerce.herokuapp.com'}
+   config.active_record.dump_schema_after_migration = false
+
+  config.active_record.dump_schema_after_migration = false
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: 587,
+    domain: "localhost",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "testing.bitterntec",
+    :password => "bittern1234"
+  }
+  config.action_mailer.raise_delivery_errors = true
 end
