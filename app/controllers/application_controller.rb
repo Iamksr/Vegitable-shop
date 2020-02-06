13aa2e51bd9550ce1914fd557d8e2cf0be20a263
Arvind Kushwah <arvindkushwah9@gmail.com>
2020-02-06 18:38:52 +0530

    fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,7 +27,6 @@ class ApplicationController < ActionController::Base
 
   def authentication_admin!
     unless current_user.is_admin?
-      flash[:alert] = "You are not authorized to perform this action."
       redirect_to(request.referrer || root_path)
     end
   end
